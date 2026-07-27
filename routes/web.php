29cac24d78f58981d422a73202f8f36deead8dac
darshanjain18 <jaindarshan1804@gmail.com>
2026-07-27 17:22:00 +0530

    Replaced all single controllers with Resource Controller
    Clean one line code contains all 7 controller

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,4 @@ use App\Http\Controllers\NoteController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
-// Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
-// Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
-// Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
-// Route::get('/notes/{id}/edit', [NoteController::class, 'edit'])->name('notes.edit');
-// Route::put('/notes/{id}', [NoteController::class, 'update'])->name('notes.update');
-// Route::delete('/notes/{id}', [NoteController::class, 'destroy'])->name('notes.destroy');
-
 Route::resource('notes', NoteController::class);
