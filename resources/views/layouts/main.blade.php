b3f8672832d974abfb657b341ba1fae701e4664b
darshanjain18 <jaindarshan1804@gmail.com>
2026-08-04 16:40:31 +0530

    integrate authentication with application navigation also downloaded breeze and made changes to original UI

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,31 +17,56 @@
             📒 Notes App
         </a>
 
-        <div class="d-flex gap-3">
+        <div class="d-flex align-items-center gap-3">
 
-            <a class="nav-link text-white" href="{{ route('about') }}">
-                @if(request()->routeIs('about'))
-                    ⭐ About 
-                @else
-                    About 
-                @endif
-            </a>
+                <a class="nav-link text-white" href="{{ route('about') }}">
+                    @if(request()->routeIs('about'))
+                        ⭐ About
+                    @else
+                        About
+                    @endif
+                </a>
 
-            <a class="nav-link text-white" href="{{ route('notes.create') }}">
-                @if(request()->routeIs('notes.create'))
-                    ⭐ Create Note
-                @else
-                    Create Note
-                @endif
-            </a>
+            @guest
+                <a class="nav-link text-white" href="{{ route('login') }}">
+                    Login
+                </a>
 
-            <a class="nav-link text-white" href="{{ route('notes.index') }}">
-                @if(request()->routeIs('notes.index'))
-                    ⭐ All Notes
-                @else
-                    All Notes
-                @endif
-            </a>
+                <a class="nav-link text-white" href="{{ route('register') }}">
+                    Register
+                </a>
+            @endguest
+
+            @auth
+
+                <a class="nav-link text-white" href="{{ route('notes.index') }}">
+                    @if(request()->routeIs('notes.index'))
+                        ⭐ All Notes
+                    @else
+                        All Notes
+                    @endif
+                </a>
+
+                <a class="nav-link text-white" href="{{ route('notes.create') }}">
+                    @if(request()->routeIs('notes.create'))
+                        ⭐ Create Note
+                    @else
+                        Create Note
+                    @endif
+                </a>
+
+                <a class="nav-link text-white" href="{{ route('profile.edit') }}">
+                    Profile
+                </a>
+
+                <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                    @csrf
+                    <button type="submit" class="btn btn-link nav-link text-white p-0 border-0">
+                        Logout
+                    </button>
+                </form>
+
+            @endauth
 
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,51 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::get('/about', [AboutController::class, 'index'])->name('about');
-Route::resource('notes', NoteController::class)->withTrashed(['show', 'edit', 'update']);
-Route::patch('/notes/{note}/restore', [NoteController::class, 'restore'])->name('notes.restore');
-Route::delete('/notes/{note}/force-delete', [NoteController::class, 'forceDelete'])->name('notes.forceDelete');
+
+/*
+|--------------------------------------------------------------------------
+| Protected Routes (Login Required)
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth')->group(function () {
+
+    // Notes Routes
+    Route::resource('notes', NoteController::class)
+        ->withTrashed(['show', 'edit', 'update']);
+
+    Route::patch('/notes/{note}/restore', [NoteController::class, 'restore'])
+        ->name('notes.restore');
+
+    Route::delete('/notes/{note}/force-delete', [NoteController::class, 'forceDelete'])
+        ->name('notes.forceDelete');
+
+    // Profile Routes
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Routes
+|--------------------------------------------------------------------------
+*/
+
+require __DIR__.'/auth.php';
